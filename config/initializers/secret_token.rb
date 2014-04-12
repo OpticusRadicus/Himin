9232fc3cb90e126329e3c9b027869e5d824a6318
Deltron <jamiedbuell@gmail.com>
2014-04-11 19:33:16 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Himin::Application.config.secret_token = 'e506921dbbe137f50dfe78c5a5f59c659a7f201593651bf37d2d95d1023d48727a0be54d10503ac487822709f7d606f14aa26f64ff8f874d8507b2f801419cba'
